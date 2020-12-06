5a8fb0b03f71db840cea60ce6ef5d8f4112b9f59
Samantha Huynh <hellosami@protonmail.com>
2020-12-06 20:59:00 +0100

    Added functions for wma and ema

--- BackTest.py
+++ BackTest.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 from binance.client import Client 
 from KlineInterval import *
+
 #from datetime import date
 
 class BackTest:
@@ -9,10 +11,10 @@ class BackTest:
     start_date = "1 Jan, 2020"
     end_date = "1 Dec, 2020"
     kline_interval = KlineInterval.ONE_DAY
-    periods_fast = [1, 2]
-    periods_slow = [5, 10, 15]
+    periods_fast = [2,4,6,8]
+    periods_slow = [10,12,15, 18]
     price_column = "Close"
-    start_capital = 100
+    indicator = "SMA"
     column_names = ['Open time', 'Open', 'High', 'Low',
                              'Close', 'Volume', 'Close time',
                              'Quote asset volume',' Number of trades',
@@ -26,50 +28,14 @@ class BackTest:
         with open(api_key_path) as f:
             self.api_key = f.read()
         with open(api_secret_path) as f:
-            self.api_secret = f.read()  
-    
-    # Setters
-    def set_symbol(self, currency_pair: str) -> None:
-        self.symbol = currency_pair
-        
-        
-    def set_start_date(self, start_date: str) -> None:
-        self.start_date = start_date
-    
-    
-    def set_end_date(self, end_date: str) -> None:
-        self.end_date = end_date
-        
-    # interval is of what type?
-    #def set_kline_interval(self, interval) -> None:
-        #self.kline_interval = KlientInterval.interval
-        
-        
-    def set_periods_fast(self, period : list) -> None:
-        self.periods_fast = period
-        
-        
-    def set_periods_slow(self, period : list) -> None:
-        self.periods_slow = period
-        
-        
-    def set_price_column(self, column_name: str) -> None:
-        self.price_column = column_name
-        
-        
-    def set_start_capital(self, capital: float) -> None:
-        self.start_capital = capital
-        
-    
-    def set_column_names(self, names: list) -> None:
-        self.column_names = names
-        
+            self.api_secret = f.read()
+            
 
     def initialize_client(self) -> None:      
         self.client = Client(self.api_key, self.api_secret)
         self.fetch_data()
         self.create_price_df()
-        self.create_sma_df()
+        self.create_ma_df()
         self.create_signals()
         
 
@@ -81,30 +47,69 @@ class BackTest:
         self.df_prices = pd.DataFrame(self.df_data)
         self.df_prices.columns = self.column_names
     
+    
+    def create_ma_df(self) -> None:
+        self.periods = set(self.periods_slow + self.periods_fast)
+        
+        if self.indicator == "SMA":
+            self.create_sma_df()
+        elif self.indicator == "EMA":
+            print("EMA found...")
+            self.create_ema_df()
+        elif self.indicator == "WMA":
+            self.create_wma_df()
+            
         
     def create_sma_df(self) -> None:
-         self.periods = set(self.periods_slow + self.periods_fast)
-         self.df_sma = pd.DataFrame()
-         for period in self.periods:
-             self.df_sma["SMA"+str(period)] = self.df_prices["Close"].rolling(period).mean()
-         self.df_sma = self.remove_rows(self.df_sma, max(self.periods))
-         
+        self.df_sma = pd.DataFrame()
+        
+        for period in self.periods:
+            self.df_sma["SMA"+str(period)] = self.df_prices["Close"].rolling(period).mean()
+            
+        self.df_sma = self.remove_rows(self.df_sma, max(self.periods))
+        
+        
+    def create_wma_df(self) -> None:
+        self.df_wma = pd.DataFrame()
+        
+        for period in self.periods:
+            weights = np.arange(1, period + 1)
+            self.df_wma["WMA"+str(period)] = self.df_prices["Close"].rolling(period).apply(lambda prices: np.dot(weights, prices)/weights.sum(), raw = True)
+        
+        self.df_wma = self.remove_rows(self.df_wma, max(self.periods))
+    
+    
+    def create_ema_df(self) -> None:
+        print("create ema")
+        self.df_ema = pd.DataFrame()
+        
+        for period in self.periods:
+            self.df_ema["EMA"+str(period)] = self.df_prices["Close"].ewm(span = period).mean()
+        
+        self.df_ema = self.remove_rows(self.df_ema, max(self.periods))
+    
         
     def remove_rows(self, dataframe: pd.DataFrame, numRows: int) -> pd.DataFrame:
-         start = numRows
-         df_removed_rows = dataframe[start:]
-         return df_removed_rows
+        start = numRows
+        df_removed_rows = dataframe[start:]
+        return df_removed_rows
         
     
     def create_signals(self) -> None:
         self.df_signals = pd.DataFrame()
         for a in self.periods_fast:
             for b in self.periods_slow:
-                if a!=b and a<b:
-                    sma_name=f"SMA({a},{b})"
-                    self.df_signals[sma_name] = self.df_sma[f"SMA{a}"] > self.df_sma[f"SMA{b}"]              
+                if a != b and a < b:
+                    if self.indicator == "SMA":
+                        sma_name=f"SMA({a},{b})"
+                        self.df_signals[sma_name] = self.df_sma[f"SMA{a}"] > self.df_sma[f"SMA{b}"]
+                    elif self.indicator == "WMA":
+                        wma_name=f"WMA({a},{b})"
+                        self.df_signals[wma_name] = self.df_wma[f"WMA{a}"] > self.df_wma[f"WMA{b}"]  
+                    elif self.indicator == "EMA":
+                        ema_name=f"EMA({a},{b})"
+                        self.df_signals[ema_name] = self.df_ema[f"EMA{a}"] > self.df_ema[f"EMA{b}"]  
 
         
-
-
+        
             
